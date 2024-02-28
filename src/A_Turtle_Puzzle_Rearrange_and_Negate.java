f6b944b6ed7bcf5945abaee8916fe340ac9e8368
Safayat <safayathossain40@gmail.com>
2024-02-29 02:18:32 +0600

    First commit

---
+++ src/A_Turtle_Puzzle_Rearrange_and_Negate.java
@@ -0,0 +1,57 @@
+// Problem: A. Turtle Puzzle: Rearrange and Negate
+// Contest: Codeforces - Codeforces Round 929 (Div. 3)
+// URL: https://codeforces.com/contest/1933/problem/A
+
+
+import java.util.Scanner;
+
+public class A_Turtle_Puzzle_Rearrange_and_Negate {
+
+    static Scanner in = new Scanner(System.in);
+
+    static int testCases, n;
+
+    static long a[];
+
+    static void solve() {
+
+        long sum = 0L;
+
+        for(long i : a) {
+
+            sum += Math.abs(i);
+
+        }
+
+        System.out.println(sum);
+
+    }
+
+    public static void main(String [] amit) {
+
+        testCases = in.nextInt();
+
+        for(int t = 0; t < testCases; ++t) {
+
+            input();
+            solve();
+
+        }
+
+    }
+
+    private static void input() {
+
+        n = in.nextInt();
+
+        a = new long[n];
+
+        for(int i = 0; i < n; ++i) {
+
+            a[i] = in.nextLong();
+
+        }
+
+    }
+
+}
---
+++ src/Square.java
@@ -0,0 +1,27 @@
+// Problem: A. Square
+// Contest: Codeforces - Codeforces Round 920 (Div. 3)
+// URL: https://codeforces.com/problemset/problem/1921/A
+import java.util.Scanner;
+
+import static java.lang.Math.pow;
+
+public class Square {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for(int i = 1; i<=t;i++){
+            int corner[][] = new int[4][2];
+            for(int j=0;j<4;j++){
+                corner[j][0] = sc.nextInt();
+                corner[j][1] = sc.nextInt();
+            }
+
+            int side= (int) Math.sqrt(Math.pow(corner[0][0] - corner[2][0], 2) + Math.pow(corner[0][1] - corner[2][1], 2));
+
+            int area=side*side;
+            System.out.println(area);
+        }
+
+    }
+}
